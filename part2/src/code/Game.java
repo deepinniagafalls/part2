67de50700b27cdf96d1ceb39fd05e8f816d7f936
jasonkimik <jaeheunk@buffalo.edu>
2015-03-27 17:39:40 -0400

    try and catch

--- part2/src/code/Game.java
+++ part2/src/code/Game.java
@@ -3,10 +3,13 @@ package code;
 public class Game {
 
 	public static void main(String[] args) {
+		try{
 		Scrabble scrabble = new Scrabble();
 		Frame f = new Frame(scrabble);
-		// TODO Auto-generated method stub
+		}
+		catch(NullPointerException e){
+			System.out.println("Fix this later");
+		}
 
 	}
-
 }
