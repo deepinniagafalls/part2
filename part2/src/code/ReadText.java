a69e67273eb877c8ff0b1dfd6bd479bbab7a07d6
mjszymko <mszymkowskicca2013@gmail.com>
2015-03-30 17:39:56 -0400

    hi

---
+++ part2/src/code/ReadText.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class ReadText {
+	public static HashMap<String,Integer> accept4 (String filePath){
+		ArrayList<String> map = new ArrayList<String>();
+		Scanner sc = new Scanner(//addfilepath);
+		
+		while(sc.hasNext()){
+			String s = sc.next();
+			map.add(s);
+		}
+	}
+}
