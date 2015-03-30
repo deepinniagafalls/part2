3dbe27f2fb3233db33b94c3fdd9c9d4400ac8d5d
jasonkimik <jaeheunk@buffalo.edu>
2015-03-30 17:44:48 -0400

    removed needless imports

--- part2/src/code/ReadText.java
+++ part2/src/code/ReadText.java
@@ -1,19 +1,17 @@
 package code;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class ReadText {
 	public static ArrayList<String> accept4 (){
-		ArrayList<String> map = new ArrayList<String>();
+		ArrayList<String> list = new ArrayList<String>();
 		Scanner sc = new Scanner("//part2//src//code//util//words");
 		
 		while(sc.hasNext()){
 			String s = sc.next();
-			map.add(s);
+			list.add(s);
 		}
-		return map;
+		return list;
 	}
 }
