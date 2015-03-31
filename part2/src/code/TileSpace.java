14313ef51375383fd8274e1f98c96dcc7a52ffe2
mjszymko <mszymkowskicca2013@gmail.com>
2015-03-31 12:03:48 -0400

    new class

---
+++ part2/src/code/TileSpace.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class TileSpace extends JButton implements ActionListener {
+
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
