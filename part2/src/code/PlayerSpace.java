3a249a5cb6281c334add318a1f42ebbfb6e453ea
Chris Elman <ceelman@buffalo.edu>
2015-04-01 17:32:42 -0400

    Try 2

--- part2/src/code/PlayerSpace.java
+++ part2/src/code/PlayerSpace.java
@@ -1,5 +1,16 @@
 package code;
 
-public class PlayerSpace {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class PlayerSpace extends JButton implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
