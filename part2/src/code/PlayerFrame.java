e6dcfc50d515972b9103effa99390d56d651038a
Chris Elman <ceelman@buffalo.edu>
2015-04-01 17:11:11 -0400

    PlayerFrame

--- part2/src/code/PlayerFrame.java
+++ part2/src/code/PlayerFrame.java
@@ -26,10 +26,10 @@ public class PlayerFrame {
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(200, 200);
-		GridLayout grid = new GridLayout(2, 2, 0, 0);
+		GridLayout grid = new GridLayout(7, 0, 0, 0);
 		frame.setLayout(grid);
 		
-		for(int i=0; i<4; i++){
+		for(int i=0; i<7; i++){
 			JButton j = new TileSpace();
 			frame.add(j);
 		}
