a2ae71ebda74b0bd98a198477936f7f7864839ca
tyler_000 <tyler_000@TylerDietrich>
2015-04-01 17:40:20 -0400

    Possible Save/Restore feature

---
+++ part2/src/code/SaveGame.java
@@ -0,0 +1,41 @@
+package code;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+public class SaveGame {
+	
+	public static void main( String arg[]){
+		SaveGame s = new SaveGame();
+	}
+
+	public void Save(String s){
+		
+		String save = s;
+		save.concat("Scrabble.java");
+		 try {
+		        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(save));
+		        oos.writeObject(oos);
+		        oos.close();
+		    } catch(Exception ex) {
+		        ex.printStackTrace();
+		    }
+	}
+	public void Load(String s){
+		
+	    if(s.contains("Scrabble.java")){
+	        try {
+	        	ObjectInputStream ois = new ObjectInputStream(new FileInputStream(s));
+	        	SaveGame sg = (SaveGame)ois.readObject();
+	        	ois.close();
+	        } catch(Exception ex) {
+	            ex.printStackTrace();
+	        }
+	    }
+	}
+}
+
+
