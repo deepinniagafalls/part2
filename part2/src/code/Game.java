b0d68ce00ea040610d15ce0722cbbb1793847698
jasonkimik <jaeheunk@buffalo.edu>
2015-04-03 14:33:32 -0400

    commit

--- part2/src/code/Game.java
+++ part2/src/code/Game.java
@@ -7,6 +7,11 @@ import code.base.Scrabble;
 import code.util.ReaderTool;
 
 public class Game {
+	Scrabble _s = new Scrabble();
+	Inventory _i = new Inventory();
+	BoardFrame _f = new BoardFrame(null,null,null);
+	PlayerFrame _tr = new PlayerFrame(null);
+	FileChooser _fc = new FileChooser();
 
 	public static void main(String[] args) {
 		Scrabble scrabble = new Scrabble();
@@ -14,5 +19,20 @@ public class Game {
 		BoardFrame f = new BoardFrame(scrabble, scrabble.getBoard(), invent);
 		PlayerFrame tr = new PlayerFrame(scrabble);
 		FileChooser fc = new FileChooser();
+	}
+	public Scrabble getScrabble(){
+		return _s;
+	}
+	public Inventory getInventory(){
+		return _i;
+	}
+	public BoardFrame getBoardFrame(){
+		return _f;
+	}
+	public PlayerFrame getPF(){
+		return _tr;
+	}
+	public FileChooser getFC(){
+		return _fc;
 	}
 }
