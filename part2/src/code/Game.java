a90f4efd4616255a590bed0795d2847c7149da21
jasonkimik <jaeheunk@buffalo.edu>
2015-04-03 14:39:18 -0400

    get

--- part2/src/code/Game.java
+++ part2/src/code/Game.java
@@ -8,19 +8,27 @@ import code.base.Scrabble;
 import code.util.ReaderTool;
 
 public class Game {
-	Scrabble _s = new Scrabble();
-	Inventory _i = new Inventory();
-	BoardFrame _f = new BoardFrame(null,null,null);
-	PlayerFrame _tr = new PlayerFrame(null);
-	FileChooser _fc = new FileChooser();
+	static Scrabble _s = new Scrabble();
+	static Inventory _i = new Inventory();
+	static Board _board = new Board();
+	static BoardFrame _f = new BoardFrame(null,null,null);
+	static PlayerFrame _tr = new PlayerFrame(null);
+	static FileChooser _fc = new FileChooser();
 
 	public static void main(String[] args) {
 		Scrabble scrabble = new Scrabble();
 		Inventory invent = scrabble.getInv();
-		Board _board = scrabble.getBoard();
-		BoardFrame f = new BoardFrame(scrabble, _board , invent);
+		Board board = scrabble.getBoard();
+		BoardFrame f = new BoardFrame(scrabble, board , invent);
 		PlayerFrame tr = new PlayerFrame(scrabble);
 		FileChooser fc = new FileChooser();
+		_s = scrabble;
+		_i = invent;
+		_board = board;
+		_f = f;
+		_tr = tr;
+		_fc = fc;
+		
 	}
 	public Scrabble getScrabble(){
 		return _s;
