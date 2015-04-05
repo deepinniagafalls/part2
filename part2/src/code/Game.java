2069d6a8b3b6db5497133c9f1c4778baedb19611
mjszymko <mszymkowskicca2013@gmail.com>
2015-04-05 17:16:53 -0400

    MAJOR CHANGES.
    added the functionality of removing tiles from the tile rack and placing
    them on the board.

--- part2/src/code/Game.java
+++ part2/src/code/Game.java
@@ -8,31 +8,27 @@ import javax.swing.UnsupportedLookAndFeelException;
 import code.base.Board;
 import code.base.Inventory;
 import code.base.Scrabble;
+import code.base.Tile;
 import code.util.ReaderTool;
 
 
 public class Game {
-	static Scrabble _s = new Scrabble();
-	static Inventory _i = new Inventory();
-	static Board _board = new Board();
-	static FileChooser _fc = new FileChooser();
 
+	Tile _t;
+	
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		Scrabble scrabble = new Scrabble();
 		Inventory invent = scrabble.getInv();
 		Board board = scrabble.getBoard();
-		BoardFrame f = new BoardFrame(scrabble, board , invent);
-		PlayerFrame tr = new PlayerFrame(scrabble);
-		_s = scrabble;
-		_i = invent;
-		_board = board;
+		PlayerFrame tr = new PlayerFrame(scrabble, scrabble.returnPlayer(0).getTileRack());
+		//PlayerFrame tr2 = new PlayerFrame(scrabble,scrabble.returnPlayer(1).getTileRack());
+		BoardFrame f = new BoardFrame(scrabble, board , invent, tr);
+		System.out.println(scrabble.returnPlayer(1));
 
+		
 	}
-	public Scrabble getScrabble(){
-		return _s;
+	
+	public Game(){
+		
 	}
-	public Inventory getInventory(){
-		return _i;
-	}
-
 }
--- part2/src/code/TileSpace.java
+++ part2/src/code/TileSpace.java
@@ -21,23 +21,34 @@ public class TileSpace extends JButton implements ActionListener {
 	private Tile _t;
 	private Inventory _i;
 	private ArrayList<Player> _players;
+	private Scrabble _scrabble;
+	private BoardFrame _bf;
 	
 	
-	public TileSpace(int x, int y, Scrabble s){
+	public TileSpace(int x, int y, Scrabble s, BoardFrame bf){
 		_j = this;
+		_bf = bf;
 		_row = x;
 		_col = y;
 		_b = s.getBoard();
 		_i = s.getInv();
 		_players = s.getPlayer();
+		_scrabble = s;
+		
 		this.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
 				//Tile t = _i.removeRandomTile();
-				Tile t = _players.get(0).getTileRack().removeTile(0);
-				_b.addTile(t, _row, _col);
-				String temp = Character.toString(t.getChar());
+				if(_b.getTempTile() != null){
+				_b.addTile(_b.getTempTile(),_row,_col);
+				_t = _b.getTile(_row,_col);
+				String temp = Character.toString(_t.getChar());
 				_j.setText(temp);
+				_b.setTempTile(null);
+				_t = _scrabble.getInv().removeRandomTile();
+				_bf.getPlayerFrame().getCurrent().setText(Character.toString(_t.getChar()));
+				_bf.getPlayerFrame().getCurrent().setCurrentTile(_t);
+				}
 			}
 		});
 	}
@@ -49,12 +60,17 @@ public class TileSpace extends JButton implements ActionListener {
 	public int getCol(){
 		return _col;
 	}
+	
+	public Tile getTile(){
+		return _t;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
+	
 	
 
 }
