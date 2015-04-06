2829a3ab13794fd27e2efcca843251f297d3e3c7
ceelman <ceelman@users.noreply.github.com>
2015-04-06 19:01:33 -0400

    Update FileChooser.java

--- part2/src/code/FileChooser.java
+++ part2/src/code/FileChooser.java
@@ -14,6 +14,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 
+import code.base.Scrabble;
+import code.base.Tile;
+
 public class FileChooser extends JFrame {
 
 	/**
@@ -25,8 +28,10 @@ public class FileChooser extends JFrame {
 
 	private JButton open = new JButton("Open"), save = new JButton("Save");
 	private JButton pass = new JButton("Pass");
+	private BoardFrame _bf;
+	private Scrabble _scrabble;
 
-	public FileChooser() {
+	public FileChooser(Scrabble scrabble, BoardFrame bf) {
 	       try {
 	            UIManager.setLookAndFeel(new javax.swing.plaf.metal.MetalLookAndFeel());
 	        } catch(Exception e) {
@@ -35,6 +40,8 @@ public class FileChooser extends JFrame {
 	       
 	    JFrame frame = new JFrame();   
 		JPanel p = new JPanel();
+		_scrabble = scrabble;
+		_bf = bf;
 		
 		p.add(filename);
 		p.add(dir);
@@ -52,7 +59,7 @@ public class FileChooser extends JFrame {
 		dir.setEditable(false);
 		filename.setEditable(false);
 		//p.setLayout(new GridLayout(2, 1));
-		cp.add(p, BorderLayout.SOUTH);
+		cp.add(p, BorderLayout.NORTH);
 		
 		
 		frame.add(p);
@@ -95,21 +102,25 @@ public class FileChooser extends JFrame {
 		}
 	}
 
-	public static void main(String[] args) {
-		run(new FileChooser(), 250, 110);
-	}
-
 	public static void run(JFrame frame, int width, int height) {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(width, height);
 		frame.setVisible(true);
 	}
+	
 	public class PassT implements ActionListener {
 		
 		public void actionPerformed(ActionEvent e) {
-			JOptionPane.showMessageDialog(null, "You have passed your turn", "Turn", JOptionPane.PLAIN_MESSAGE);
+			PlayerFrame temp = _bf.getPlayerFrame(0);
+			for(int i=0; i<12; i++){
+				if(temp.getPlayerSpace(i)==null){
+					Tile t = _scrabble.getInv().removeRandomTile();
+					temp.getPlayerSpace(i).setText(Character.toString(t.getChar()));
+					temp.getPlayerSpace(i).setCurrentTile(t);
+				}
+			}
+			JOptionPane.showMessageDialog(null, "You have passed your turn");
 	}
 	}
 
 }
-
