72efd8775e933aebcc8c8a84688f24fd3efe9ad3
jasonkimik <jaeheunk@buffalo.edu>
2015-04-07 12:33:11 -0400

    þþþþþ

--- part2/src/code/util/þeRandomColor.java
+++ part2/src/code/util/þeRandomColor.java
@@ -2,16 +2,16 @@ package code.util;
 
 import java.util.Random;
 
-public class þheRandomColor {
+public class þeRandomColor {
 	
-	public þheRandomColor(float r2, float g2, float b2){
+	public þeRandomColor(float r2, float g2, float b2){
 		Random random = new Random();
 		
 		float r = random.nextFloat();
 		float g = random.nextFloat();
 		float b = random.nextFloat();
 		
-		þheRandomColor randomcolor = new þheRandomColor(r, g, b);
+		þeRandomColor randomcolor = new þeRandomColor(r, g, b);
 	}
 
 }
