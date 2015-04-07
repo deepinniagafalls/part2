5181b826e7f187f7d2e0d8d2a31f9bc402443642
mjszymko <mszymkowskicca2013@gmail.com>
2015-04-07 17:41:06 -0400

    driver class

---
+++ part2/src/code/Driver.java
@@ -0,0 +1,9 @@
+package code;
+
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Driver {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		Game g = new Game();
+	}
+}
