7b931aca51009a770ffbfbc794cb05a417c3afb9
mjszymko <mszymkowskicca2013@gmail.com>
2015-04-08 15:39:44 -0400

    filewriting

--- part2/src/code/SaveGame.java
+++ part2/src/code/SaveGame.java
@@ -1,25 +1,52 @@
 package code;
 
 import java.awt.FileDialog;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.JFrame;
 
 
 @SuppressWarnings("unused")
 public class SaveGame {
 	
-	public static void main(String[] args){
+	private String _fileToWriteTo = "C:\\Users\\Michael\\myfile.txt";
+	
+	public static void main(String[] args) throws IOException{
 		SaveGame t = new SaveGame();
+		t.writeNewFile();
+		
 	}
 	
-	public SaveGame(){
+	public SaveGame() throws IOException{
+		/*
 		JFrame j = new JFrame();
-		FileDialog chooser = new FileDialog(j);
+		FileDialog chooser = new FileDialog(j,"Save your file",FileDialog.SAVE);
+		//FileDialog chooser = new FileDialog(j,"Save your file",FileDialog.LOAD);
 		chooser.setDirectory("C:\\");
 		chooser.setFile("*.txt");
 		chooser.setVisible(true);
 		String filename = chooser.getFile();
 		String path = chooser.getDirectory();
 		System.out.println(path);
+		String filetoWrite = path + filename;
+		_fileToWriteTo = filetoWrite;
+		//File file = new File("/users/mkyong/filename.txt");
+		*/
+		String content = "YOOOOOOOOOOOOOOOOOOOOOOOo";
+		File file = new File(_fileToWriteTo);
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(content);
+		bw.close();
+		System.out.println("Done");
+		
 	}
+	
+	public void writeNewFile() throws IOException{
+		
+		
+	}
+	
 }
