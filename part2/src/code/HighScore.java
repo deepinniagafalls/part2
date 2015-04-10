128cbaf33e9c0444c188b6f4b6664789a1c9df7f
Jason <Jason@Jason-PC>
2015-04-10 16:36:00 -0400

    HighScore class has more features

--- part2/src/code/HighScore.java
+++ part2/src/code/HighScore.java
@@ -1,23 +1,38 @@
 package code;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import code.base.Player;
 
 public class HighScore {
 	ArrayList<Player> _p;
+	ArrayList<String> _words;
 	public HighScore(){
 	}
 	public void endGame(ArrayList<Player> p) throws FileNotFoundException, UnsupportedEncodingException{
 		// Load previous high score
-		
+		JOptionPane.showMessageDialog(null, "The game has ended");
+		_words = new ArrayList<String>();
+	    String  _line = null;
+	    try{
+	       BufferedReader br = new BufferedReader(new FileReader("res/words.txt"));
+	       while ((_line = br.readLine()) != null) {
+	          _words.add(_line);
+	       }       
+	    }
+	    catch(Exception e){}
+	    	
 		// Save current high score with the past one
+	    int pn = p.size();
 		PrintWriter writer = new PrintWriter("res/save.txt", "UTF-8");
 		writer.println("The first line");
 		writer.println("The second line");
