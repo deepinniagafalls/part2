072ff735e17dcd62a6aa9738c425fa74a8f4bdbb
jasonkimik <jaeheunk@buffalo.edu>
2015-04-09 23:25:00 -0400

    HighScore

---
+++ part2/src/code/HighScore.java
@@ -0,0 +1,16 @@
+package code;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import code.base.Player;
+
+public class HighScore {
+	JFrame a = new JFrame("High Score");
+	public HighScore(ArrayList<Player> _p){
+	}
+	public void updateBoard(){
+		
+	}
+}
