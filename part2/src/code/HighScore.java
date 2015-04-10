2ec0ea8f324ecaa475d915af55e105d01c78dad8
jasonkimik <jaeheunk@buffalo.edu>
2015-04-09 23:33:14 -0400

    commit

--- part2/src/code/HighScore.java
+++ part2/src/code/HighScore.java
@@ -3,14 +3,18 @@ package code;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import code.base.Player;
 
 public class HighScore {
-	JFrame a = new JFrame("High Score");
-	public HighScore(ArrayList<Player> _p){
+	JFrame _a = new JFrame("High Score");
+	ArrayList<Player> _p;
+	public HighScore(){
 	}
-	public void updateBoard(){
-		
+	public void updateBoard(ArrayList<Player> p){
+		_p = p;
+		JLabel score = new JLabel("High Scores:");
+		_a.add(score);
 	}
 }
